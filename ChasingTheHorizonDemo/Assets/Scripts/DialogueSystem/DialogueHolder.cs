6daca7c582307fe4b634b3546010a2dfcef43f4c
Coolrelia <sammyrludwig@gmail.com>
2021-02-03 16:47:39 -0500

    Added the Dialogue System
    title

---
+++ ChasingTheHorizonDemo/Assets/Scripts/DialogueSystem/DialogueHolder.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DialogueSystem
+{
+    public class DialogueHolder : MonoBehaviour
+    {
+        [Header("Dialogue Boxs")]
+        [SerializeField] private Vector3 box1;
+        [SerializeField] private Vector3 box2;
+
+        private void Awake()
+        {
+            StartCoroutine(DialogueSequence());
+        }
+
+        private IEnumerator DialogueSequence()
+        {
+            for(int i=0; i<transform.childCount; i++)
+            {
+                Deactivate();
+                transform.GetChild(i).gameObject.SetActive(true);
+                if(transform.GetChild(i).GetComponent<DialogueLine>().textBox1 == true)
+                {
+                    transform.GetChild(i).GetComponent<RectTransform>().localPosition = box1;
+                }
+                else
+                {
+                    transform.GetChild(i).GetComponent<RectTransform>().localPosition = box2;
+                }
+                yield return new WaitUntil(() => transform.GetChild(i).GetComponent<DialogueLine>().finished);
+            }
+        }
+
+        private void Deactivate()
+        {
+            for(int i=0; i<transform.childCount; i++)
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ ChasingTheHorizonDemo/Assets/Scripts/DialogueSystem/DialogueLine.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+
+        [Header("Text Options")]
+        [SerializeField] private string input;
+        [SerializeField] private Color textColor;
+        [SerializeField] private Font textFont;
+
+        [Header("Time Parameters")]
+        [SerializeField] private float delay;
+
+        [Header("Sound")]
+        [SerializeField] private AudioClip sound;
+
+        [Header("Portrait")]
+        [SerializeField] private Sprite portraitSprite;
+        [SerializeField] private Image imageHolder;
+
+        [Header("Textbox")]
+        public bool textBox1;
+        public bool textBox2;
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+            textHolder.text = "";
+
+            imageHolder.sprite = portraitSprite;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay, sound));
+        }
+    }
+}
