07ce7f2ddf3e5752bfa36ac6b4d6fa4e836c8a93
Volki813 <volkan1234@outlook.de>
2023-04-29 19:43:01 +0200

    passive skill system foundation

---
+++ ChasingTheHorizonDemo/Assets/Scripts/ScriptableObjects/Skills/PassiveSkills/PassiveSkills.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Skills/Passive Skill")]
+public class PassiveSkills : ScriptableObject
+{
+    // increase or reduce stats (use negative numbers for reducing stats
+    // leave at 0 if stats should stay unchanged
+    public int healthAmount;
+    public int strengthAmount;
+    public int magicAmount;
+    public int defenseAmount;
+    public int resistanceAmount;
+    public int proficiencyAmount;
+    public int motivationAmount;
+    public int agilityAmount;
+    public int schmovementAmount;
+}
---
+++ ChasingTheHorizonDemo/Assets/Scripts/ScriptableObjects/Skills/SkillLoader.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class SkillLoader : MonoBehaviour
+{
+
+    // place on unit and then drag passives here
+    [SerializeField] private PassiveSkills[] passives;
+
+    private void OnEnable()
+    {
+        // loads passives
+
+        UnitLoader unit = GetComponent<UnitLoader>();
+
+        foreach (PassiveSkills passive in passives)
+        {
+            if (unit == null) break;
+
+            unit.unit.statistics.health += passive.healthAmount;
+            unit.unit.statistics.strength += passive.strengthAmount;
+            unit.unit.statistics.magic += passive.magicAmount;
+            unit.unit.statistics.defense += passive.defenseAmount;
+            unit.unit.statistics.resistance += passive.resistanceAmount;
+            unit.unit.statistics.proficiency += passive.proficiencyAmount;
+            unit.unit.statistics.motivation += passive.motivationAmount;
+            unit.unit.statistics.agility += passive.agilityAmount;
+            unit.unit.statistics.movement += passive.schmovementAmount;
+        }
+    }
+
+    private void OnDisable()
+    {
+        // unloads passives
+
+        UnitLoader unit = GetComponent<UnitLoader>();
+
+        foreach (PassiveSkills passive in passives)
+        {
+            if (unit == null) break;
+
+            unit.unit.statistics.health -= passive.healthAmount;
+            unit.unit.statistics.strength -= passive.strengthAmount;
+            unit.unit.statistics.magic -= passive.magicAmount;
+            unit.unit.statistics.defense -= passive.defenseAmount;
+            unit.unit.statistics.resistance -= passive.resistanceAmount;
+            unit.unit.statistics.proficiency -= passive.proficiencyAmount;
+            unit.unit.statistics.motivation -= passive.motivationAmount;
+            unit.unit.statistics.agility -= passive.agilityAmount;
+            unit.unit.statistics.movement -= passive.schmovementAmount;
+
+        }
+    }
+}
