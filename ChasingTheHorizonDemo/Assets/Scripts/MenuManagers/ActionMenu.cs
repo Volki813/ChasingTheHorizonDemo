115006efcdbddf46771f37d405fd7983a3b1218d
Volki813 <volkan1234@outlook.de>
2024-01-01 17:23:28 +0100

    ActionsMenu added
    ActionsMenu takes in button prefabs and instantiates them in a vertical list (disabling it doesn't remove the list yet)

---
+++ ChasingTheHorizonDemo/Assets/Scripts/MenuManagers/ActionMenu.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ActionMenu : MonoBehaviour
+{
+    [SerializeField] private List<Button> buttonList;
+    private List<Button> instantiatedButtonList = new List<Button>();
+    
+
+    private void OnEnable()
+    {
+        foreach (Button button in buttonList)
+        {
+            Button b = Instantiate(button, transform);
+            instantiatedButtonList.Add(b);
+            b.Select();
+        }
+    }
+
+    //private void OnDisable()
+    //{
+    //    foreach (Button button in instantiatedButtonList)
+    //    {
+    //        Destroy(button);
+    //    }
+    //}
+}
