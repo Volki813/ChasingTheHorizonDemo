fad42f9845699559cc11296a79924fc91b46b60f
Volki813 <volkan1234@outlook.de>
2024-01-03 18:39:49 +0100

    ActionsMenu base finished

--- ChasingTheHorizonDemo/Assets/Scripts/MenuManagers/ActionMenu.cs
+++ ChasingTheHorizonDemo/Assets/Scripts/MenuManagers/ActionMenu.cs
@@ -4,17 +4,29 @@ using System.Collections.Generic;
 
 public class ActionMenu : MonoBehaviour
 {
-    [SerializeField] private List<Button> buttonList;
-    private List<Button> instantiatedButtonList = new List<Button>();
+    // [SerializeField] private List<Button> buttonList;
+    // private List<Button> instantiatedButtonList = new List<Button>();
     
 
     private void OnEnable()
     {
-        foreach (Button button in buttonList)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            Button b = Instantiate(button, transform);
-            instantiatedButtonList.Add(b);
-            b.Select();
+            transform.GetChild(i).gameObject.SetActive(true);
+        }
+        //foreach (Button button in buttonList)
+        //{
+        //    Button b = Instantiate(button, transform);
+        //    instantiatedButtonList.Add(b);
+        //    b.Select();
+        //}
+    }
+
+    private void OnDisable()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
         }
     }
 
