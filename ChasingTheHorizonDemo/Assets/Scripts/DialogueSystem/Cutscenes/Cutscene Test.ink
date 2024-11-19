eeb69945511c020ba9bf889ca590ff70fdb1cc78
Volkan <volkan1234@outlook.de>
2024-11-20 00:56:18 +0100

    basic dialogue with choices

---
+++ ChasingTheHorizonDemo/Assets/Scripts/DialogueSystem/Cutscenes/Cutscene Test.ink
@@ -0,0 +1,14 @@
+-> main
+
+=== main ===
+Which Pokemon do you choose?
+    + [Charmander]
+        -> chosen("Charmander")
+    + [Bulbasaur]
+        -> chosen("Bulba")
+    + [Squirtle]
+        -> chosen("Squirt")
+
+=== chosen(pokemon) ===
+You chose {pokemon}!
+-> END
---
+++ ChasingTheHorizonDemo/Assets/Scripts/DialogueSystem/Cutscenes/DialogueManagerWithInk.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+using TMPro;
+using Ink.Runtime;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DialogueManagerWithInk : MonoBehaviour
+{
+    [Header("Dialogue UI")]
+    [SerializeField] private GameObject dialoguePanel = null;
+    [SerializeField] private TextMeshProUGUI dialogueText = null;
+
+    [Header("Choices UI")]
+    [SerializeField] private GameObject[] choices = null;
+    private TextMeshProUGUI[] choicesText = null;
+
+    private Story currentStory;
+
+    public bool dialogueIsPlaying { get; private set; }
+
+    public static DialogueManagerWithInk instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("More than one Dialogue Manager in the scene!");
+        }
+        instance = this;
+    }
+
+    private void Start()
+    {
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+
+        // Get choices texts
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach (GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
+    }
+
+    private void Update()
+    {
+        if (!dialogueIsPlaying) return;
+
+        // Continue Story () when next is pressed, too lazy to set up the input system here rn
+    }
+
+    public void EnterDialogueMode(TextAsset inkJSON)
+    {
+        currentStory = new Story(inkJSON.text);
+        dialogueIsPlaying = true;
+        dialoguePanel.SetActive(true);
+
+        ContinueStory();
+    }
+
+    private IEnumerator ExitDialogueMode()
+    {
+        yield return new WaitForSeconds(0.2f);
+
+        dialogueIsPlaying = false;
+        dialoguePanel.SetActive(false);
+        dialogueText.text = "End of Dialogue";
+    }
+
+    public void ContinueStory()
+    {
+        if (currentStory.canContinue)
+        {
+            // set text for current line
+            dialogueText.text = currentStory.Continue();
+            // display choices for current line
+            DisplayChoices();
+        }
+        else
+        {
+            StartCoroutine(ExitDialogueMode());
+        }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        // make sure the UI can support the amount of choices
+        if (currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices than the UI can support. Number of choices: " + currentChoices.Count);
+        }
+
+        int index = 0;
+        foreach (Choice choice in currentChoices)
+        {
+            choices[index].SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+        // disable remaining choices
+        for (int i = index; i < choices.Length; i++)
+        {
+            choices[i].SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    }
+
+    private IEnumerator SelectFirstChoice()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex) // set choiceIndex like an array, e.g. first choice has index 0, second is 1, etc.
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
+    }
+}
